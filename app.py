5afe0e911d35bd10dec30452af2ca04165054c2b
Eduardo MV <eduumtzv@gmail.com>
2025-05-05 19:22:00 -0600

    V.1.6

--- app.py
+++ app.py
@@ -23,5 +23,6 @@ def invitacion(codigo):
         abort(404)
     return render_template("invitacion.html", nombre=nombre)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
